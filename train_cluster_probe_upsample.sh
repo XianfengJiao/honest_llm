99b67116cf53126574e69bed5714c4d9f76e86f6
XianfengJiao <XianfengJiao@foxmail.com>
2023-12-27 18:53:07 +0800

    fix upsample

--- train_cluster_probe_upsample.sh
+++ train_cluster_probe_upsample.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 定义 probe_base_weight 和 n_clusters 的数组
-alpha=(15 10)
+alpha=(15)
 probe_base_weights=(0)
-n_clusters=(3 4)
-num_heads=(48 24)
-cut_rates=(0.75 0.9 0.5)
+n_clusters=(4)
+num_heads=(48)
+cut_rates=(0.75)
 
 # 外循环遍历 probe_base_weight
 for c in "${cut_rates[@]}"; do
@@ -18,7 +18,7 @@ for c in "${cut_rates[@]}"; do
                     echo "Running: valid_2_fold_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}"
 
                     # 构建并执行命令
-                    nohup python -u valid_2_fold_cluster_probe_upsample.py --device=0 --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" --cut_rate="$c" > "./logs/valid_2_fold_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}.log" 2>&1 &
+                    nohup python -u valid_2_fold_cluster_probe_upsample.py --device=1 --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" --cut_rate="$c" > "./logs/valid_2_fold_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}.log" 2>&1 &
                     
                     # 等待上一个命令完成
                     wait
