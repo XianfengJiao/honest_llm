42545337b9806e8980a2d81d23e4131c27629803
XianfengJiao <XianfengJiao@foxmail.com>
2024-01-13 01:58:11 +0800

    edit

--- fewshot_cluster_probe_upsample_none.sh
+++ fewshot_cluster_probe_upsample_none.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-alpha=(12 15 17)
-probe_base_weights=(0)
+alpha=(15)
+probe_base_weights=(0.4 0.5 0.6 0.7 0.8 0.9)
 n_clusters=(3)
-num_heads=(16 24 32)
-cut_rates=(0.8)
+num_heads=(32)
+cut_rates=(0.9)
 
 for c in "${cut_rates[@]}"; do
     for a in "${alpha[@]}"; do
@@ -15,7 +15,7 @@ for c in "${cut_rates[@]}"; do
                 for cluster in "${n_clusters[@]}"; do
                     # 显示正在执行的命令
                     echo "Running: fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_none"
-                    nohup python -u fewshot_cluster_probe_upsample.py --method='none' --device=2 --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" --cut_rate="$c" > "./logs/fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_prob_none.log" 2>&1 &
+                    nohup python -u fewshot_cluster_probe_upsample.py --method='none' --device=1 --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" --cut_rate="$c" > "./logs/fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_prob_none.log" 2>&1 &
                     wait
                 done
             done
