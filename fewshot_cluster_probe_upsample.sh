fef7e93fce0ce112f7c5f792f428994ac1d9a0ab
XianfengJiao <XianfengJiao@foxmail.com>
2024-01-12 12:31:07 +0800

    edit

--- fewshot_cluster_probe_upsample.sh
+++ fewshot_cluster_probe_upsample.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-alpha=(12 15)
-probe_base_weights=(0)
+alpha=(10 14 17)
+probe_base_weights=(0 0.1 0.2)
 n_clusters=(3)
-num_heads=(16 24 32)
+num_heads=(24)
 cut_rates=(0.9)
 
 for c in "${cut_rates[@]}"; do
@@ -18,9 +18,9 @@ for c in "${cut_rates[@]}"; do
                     nohup python -u fewshot_cluster_probe_upsample.py --method='icl' --device=0 --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" --cut_rate="$c" > "./logs/fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_prob_icl.log" 2>&1 &
                     wait
 
-                    echo "Running: fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_none"
-                    nohup python -u fewshot_cluster_probe_upsample.py --method='none' --device=0 --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" --cut_rate="$c" > "./logs/fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_prob_none.log" 2>&1 &
-                    wait
+                    # echo "Running: fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_none"
+                    # nohup python -u fewshot_cluster_probe_upsample.py --method='none' --device=0 --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" --cut_rate="$c" > "./logs/fewshot_llama_7B_cluster${cluster}_probe_cut${c}_heads${num_head}_alpha${a}_baseW${weight//.}_prob_none.log" 2>&1 &
+                    # wait
                 done
             done
         done
