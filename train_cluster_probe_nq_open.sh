cac2923475673cd998885dfc40cd154d6fdaf620
XianfengJiao <XianfengJiao@foxmail.com>
2024-01-21 17:13:44 +0800

    edit nq_open

--- train_cluster_probe_nq_open.sh
+++ train_cluster_probe_nq_open.sh
@@ -3,7 +3,7 @@
 alpha=(5 7 9 10)
 probe_base_weights=(0)
 n_clusters=(2)
-num_heads=(8 10 12 14 16)
+num_heads=(8 12 16 24)
 device=0
 
 for a in "${alpha[@]}"; do
@@ -15,7 +15,7 @@ for a in "${alpha[@]}"; do
                 echo "Running: nq_open_valid_1_fold_llama_7B_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}"
 
                 # 构建并执行命令
-                nohup python -u valid_cluster_probe_external.py --device=${device} --dataset_name=nq_open --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" > "./logs/0117_nq_open_valid_1_fold_llama_7B_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_prob.log" 2>&1 &
+                nohup python -u valid_cluster_probe_external.py --activation_type=cutrandom --device=${device} --dataset_name=nq_open --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" > "./logs/0121_nq_open_valid_1_fold_llama_7B_cutrandom_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_prob.log" 2>&1 &
                 
                 # 等待上一个命令完成
                 wait
