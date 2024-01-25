568af7cee1bbba6afb8f19ae076d4cc63b094288
XianfengJiao <XianfengJiao@foxmail.com>
2024-01-25 16:40:47 +0800

    edit external

--- train_cluster_probe_trivia_qa.sh
+++ train_cluster_probe_trivia_qa.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-alpha=(5 7 9 10)
+alpha=(5 7)
 probe_base_weights=(0)
 n_clusters=(2)
-num_heads=(8 12 16 24)
+num_heads=(8 16 24 32 48)
 device=0
+
+
+# echo "Running: trivia_qa_valid_1_fold_llama_7B_pure"
+# nohup python -u valid_cluster_probe_external.py --pure --dataset_name=trivia_qa --activation_type=cutrandom > ./logs/llama_7B_pure_trivia_qa.log 2>&1 &
+# wait
 
 for a in "${alpha[@]}"; do
     for num_head in "${num_heads[@]}"; do
@@ -15,7 +20,7 @@ for a in "${alpha[@]}"; do
                 echo "Running: trivia_qa_valid_1_fold_llama_7B_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}"
 
                 # 构建并执行命令
-                nohup python -u valid_cluster_probe_external.py --activation_type=cutrandom --device=${device} --dataset_name=trivia_qa --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" > "./logs/0121_trivia_qa_valid_1_fold_llama_7B_cutrandom_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_prob.log" 2>&1 &
+                nohup python -u valid_cluster_probe_external.py --activation_type=cutrandom --device=${device} --dataset_name=trivia_qa --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" > "./logs/0122_trivia_qa_valid_1_fold_llama_7B_cutrandom_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_prob.log" 2>&1 &
                 
                 # 等待上一个命令完成
                 wait
--- view_dataset.py
+++ view_dataset.py
@@ -1,6 +1,27 @@
 from datasets import load_dataset
 import pandas as pd
 
+url = "https://huggingface.co/api/datasets/openbookqa/parquet/additional/train/0.parquet"
+dataset = load_dataset('parquet', data_files=url)
+# 定义一个函数来提取正确和错误的答案
+def extract_answers(row):
+    # 获取正确答案的索引
+    correct_index = row['choices']['label'].index(row['answerKey'])
+    # 提取正确答案
+    correct_answer = row['choices']['text'][correct_index]
+    # 提取错误答案
+    incorrect_answers = [ans for i, ans in enumerate(row['choices']['text']) if i != correct_index]
+    incorrect_answers = '; '.join(incorrect_answers)
+
+    return pd.Series([correct_answer, incorrect_answers])
+
+df = pd.DataFrame(dataset['train'])
+df['Question'] = df['question_stem']
+df[['Correct Answers', 'Incorrect Answers']] = df.apply(extract_answers, axis=1)
+df['Best Answer'] = df['Correct Answers']
+print(dataset)
+
+
 url = "https://huggingface.co/api/datasets/OamPatel/iti_nq_open_val/parquet/default/validation/0.parquet"
 dataset = load_dataset('parquet', data_files=url)['train']
 
