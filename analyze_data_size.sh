ba36e3ff77d8470b32ffda31571aab296c274d3c
XianfengJiao <XianfengJiao@foxmail.com>
2024-02-09 11:23:45 +0800

    analyze data size

---
+++ analyze_data_size.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 定义 probe_base_weight 和 n_clusters 的数组
+alpha=(12)
+probe_base_weights=(0)
+n_clusters=(3)
+num_heads=(24)
+train_rate=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
+seed=0
+
+# 外循环遍历 probe_base_weight
+
+for a in "${alpha[@]}"; do
+    for num_head in "${num_heads[@]}"; do
+        for weight in "${probe_base_weights[@]}"; do
+            # 内循环遍历 n_clusters
+            for cluster in "${n_clusters[@]}"; do
+                for rate in "${train_rate[@]}"; do
+                    # 显示正在执行的命令
+                    echo "Running: analyze_data_size_llama_7B_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_train${rate}"
+
+                    # 构建并执行命令
+                    nohup python -u data_size_analysis_act.py --seed=${seed} --train_ratio="$rate" --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" > "./logs/0209_seed${seed}_analyze_data_size_llama_7B_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_prob_train${rate}.log" 2>&1 &
+                    
+                    # 等待上一个命令完成
+                    wait
+                done
+            done
+        done
+    done
+done
--- train_mean_centring_llama_7b.sh
+++ train_mean_centring_llama_7b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义 probe_base_weight 和 n_clusters 的数组
-alpha=(4 8 16)
+alpha=(24 32 48)
 layers=(25 30 15 10 5)
 
 # 外循环遍历 probe_base_weight
@@ -12,7 +12,7 @@ for a in "${alpha[@]}"; do
         echo "Running: valid_2_fold_llama_7B_alpha${a}_layer${l}"
 
         # 构建并执行命令
-        nohup python -u valid_2_fold_mean_centring.py --model_name=llama_7B --alpha="$a" --select_layer="$l" > "./logs/valid_2_fold_llama_7B_alpha${a}_layer${l}.log" 2>&1 &
+        nohup python -u valid_2_fold_mean_centring.py --model_name=llama_7B --alpha="$a" --select_layer="$l" > "./logs/mean_centring_valid_2_fold_llama_7B_alpha${a}_layer${l}.log" 2>&1 &
         
         # 等待上一个命令完成
         wait
