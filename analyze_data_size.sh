c482956684313dd3bcffb194a67c68153282a7bd
XianfengJiao <XianfengJiao@foxmail.com>
2024-02-10 16:48:24 +0800

    fix

--- analyze_data_size.sh
+++ analyze_data_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# 定义 probe_base_weight 和 n_clusters 的数组
 alpha=(14)
 probe_base_weights=(0)
 
@@ -8,7 +7,8 @@ num_heads=(32)
 train_rate=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
 seed=1
 
-# 外循环遍历 probe_base_weight
+echo "Running: analyze_data_size_llama_7B_seed${seed}_pure"
+nohup python -u data_size_analysis_act.py --seed=${seed} --pure > "./logs/0210_data_size_analysis_pure_seed${seed}" 2>&1 &
 
 for a in "${alpha[@]}"; do
     for num_head in "${num_heads[@]}"; do
