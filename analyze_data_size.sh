171e5e8f7f4e0b8933d8dec2744085326b7d1ba1
XianfengJiao <XianfengJiao@foxmail.com>
2024-03-27 18:58:03 +0800

    fix

--- analyze_data_size.sh
+++ analyze_data_size.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-alpha=(15)
+alpha=(14)
 probe_base_weights=(0)
 
-n_clusters=(3)
-num_heads=(16)
-train_rate=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
-seed=5
-model=vicuna_7B
+n_clusters=(4)
+num_heads=(32)
+train_rate=(0.7 0.8 0.9)
+seed=2
+model=llama_7B
 
-echo "Running: analyze_data_size_${model}_seed${seed}_pure"
-nohup python -u data_size_analysis_act.py --seed=${seed} --pure > "./logs/0211_${model}_data_size_analysis_pure_seed${seed}.log" 2>&1 &
-wait
+# echo "Running: analyze_data_size_${model}_seed${seed}_pure"
+# nohup python -u data_size_analysis_act.py --seed=${seed} --pure > "./logs/0211_${model}_data_size_analysis_pure_seed${seed}.log" 2>&1 &
+# wait
 
 for a in "${alpha[@]}"; do
     for num_head in "${num_heads[@]}"; do
@@ -22,7 +22,7 @@ for a in "${alpha[@]}"; do
                     echo "Running: analyze_data_size_${model}_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_train${rate}_seed${seed}"
 
                     # 构建并执行命令
-                    nohup python -u data_size_analysis_act.py --model_name=${model} --seed=${seed} --train_ratio="$rate" --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" > "./logs/0211_seed${seed}_analyze_data_size_${model}_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_prob_train${rate}.log" 2>&1 &
+                    nohup python -u data_size_analysis_act.py --model_name=${model} --seed=${seed} --train_ratio="$rate" --probe_type=prob --probe_base_weight="$weight" --n_clusters="$cluster" --num_heads="$num_head" --alpha="$a" > "./logs/0212_seed${seed}_analyze_data_size_${model}_cluster${cluster}_probe_heads${num_head}_alpha${a}_baseW${weight//.}_prob_train${rate}.log" 2>&1 &
                     
                     # 等待上一个命令完成
                     wait
